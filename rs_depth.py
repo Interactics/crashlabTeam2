337ca7e8a2f9cc566bea982c17d386ba1779393b
yhyingit <56077549+yhyingit@users.noreply.github.com>
2019-11-29 02:01:36 +0900

    print Depth in bounding box

--- rs_depth.py
+++ rs_depth.py
@@ -1,75 +1,93 @@
 #!/usr/bin/env python
 
-# This node for Target Selection 
-# SUB Depth, boudning box 
-# PUB selected person's Bounding BOX 
-#- /camera/color/image_raw
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
-##########################
+#####################################################
+##              Align Depth to Color               ##
+#####################################################
 
-##########################
-
-##########################
-
-#import ros
+# First import the library
 import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
 import numpy as np
+# Import OpenCV for easy image rendering
 import cv2
-import math
-
-def getVerticalCoordinate(y,distance):
-    # realsense RGB : FOV 60.4 x 42.5 x 77 (H V D)
-    # realsense Depth : FOV 73 x 58 x 95 (H V D)
-    VFov2 = math.radians(42.5 / 2)
-    VSize = math.tan(VFov2) * 2
-    Vcenter = (height -1 ) /2 
-    VPixel = VSize/(height - 1)
-    VRatio = (VCenter - y) * VPixel
-    return distance * VRatio
-
-def getHorizontalCoordinate(x, distance):
-    # realsense RGB : FOV 60.4 x 42.5 x 77 (H V D)
-    # realsense Depth : FOV 73 x 58 x 95 (H V D)
-    HFov2 = math.radians(69.4 / 2)
-    HSize = math.tan(HFov2) * 2
-    Hcenter = (width -1 ) /2 
-    HPixel = HSize/(width - 1)
-    HRatio = (x - width) * HPixel
-    return distance * HRatio   
 
 
-# Bounding box in ROI
-bbox =  (50, 50, 100, 100)
 
-pipeline =rs.pipeline()
-config =rs.config()
+# Create a pipeline
+pipeline = rs.pipeline()
+
+#Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8,30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+bbox =  (200, 200, 300, 300)
 
-pipeline.start(config)
+# Start streaming
+profile = pipeline.start(config)
 
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+# Streaming loop
 try:
     while True:
-
+        # Get frameset of color and depth
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
+        # frames.get_depth_frame() is a 640x360 depth image
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
             continue
 
-        depth_image = np.asanyarray(depth_frame.get_data())
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
+        # Render images
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
         images = np.hstack((color_image, depth_colormap))
-        cv2.circle(images,(300,200),5,(0,0,255),-1)
-        print depth_frame.get_distance(300,300)
-        cv2.namedWindow('RS', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+        
+        ###measure the Depth
+        cv2.circle(images,(300,300),5,(0,0,255),-1)
+        cv2.rectangle(images, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+
+        #measured_distance = aligned_depth_frame.get_distance(300,300)
+    
+        distance=0
+        for y in range(bbox[1], bbox[3]) :
+            for x in range(bbox[0],bbox[2]) : 
+                distance=distance+aligned_depth_frame.get_distance(x,y)
+        
+        mean_dist = distance/((bbox[3]-bbox[1])*(bbox[2]-bbox[0]))
+        print round(mean_dist,2)
+        #print round(measured_distance,2)
 
 
-finally :
 
+        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('Align Example', images)
+        key = cv2.waitKey(1)
+        # Press esc or 'q' to close the image window
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+finally:
     pipeline.stop()
